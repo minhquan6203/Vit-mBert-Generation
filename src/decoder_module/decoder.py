80a3e1697623d9e8eaf26da67e1989fbd3676087
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-17 12:53:10 +0700

    fix

--- src/decoder_module/decoder.py
+++ src/decoder_module/decoder.py
@@ -10,13 +10,15 @@ class Decoder(nn.Module):
     def __init__(self, config):
         super(Decoder, self).__init__()
         self.gen = BertGenerationDecoder.from_pretrained(config['decoder']['text_decoder'])
-        
+        self.layer_norm = nn.LayerNorm(config["decoder"]['d_model'])
     def train_forward(self,encoder_features: torch.Tensor, encoder_attention_mask: torch.Tensor, answer_ids: torch.Tensor=None):
-        outputs = self.gen(inputs_embeds=encoder_features, attention_mask=encoder_attention_mask, labels=answer_ids)
+        outputs = self.layer_norm(encoder_features)
+        outputs = self.gen(inputs_embeds=outputs, attention_mask=encoder_attention_mask, labels=answer_ids)
         return outputs.logits, outputs.loss
 
     def eval_forward(self, encoder_features: torch.Tensor, encoder_attention_mask: torch.Tensor, answer_ids: torch.Tensor=None):
         for param in self.gen.parameters():
             param.requires_grad = False
-        outputs = self.gen(inputs_embeds=encoder_features, attention_mask=encoder_attention_mask, labels=answer_ids)
+        outputs = self.layer_norm(encoder_features)
+        outputs = self.gen(inputs_embeds=outputs, attention_mask=encoder_attention_mask, labels=answer_ids)
         return outputs.logits, outputs.loss
