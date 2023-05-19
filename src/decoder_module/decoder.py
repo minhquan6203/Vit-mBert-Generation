6764f12f512cf9be1e9a821e734afe4f6bce1016
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-19 14:18:05 +0700

    fix

--- src/decoder_module/decoder.py
+++ src/decoder_module/decoder.py
@@ -22,16 +22,13 @@ from mask.masking import generate_padding_mask, generate_sequential_mask, genera
 class DecoderLayer(nn.Module):
     def __init__(self, config):
         super(DecoderLayer, self).__init__()
-
         self.self_attn = MultiHeadAtt(config)
-        self.enc_attn = MultiHeadAtt(config)
         self.pwff = PositionWiseFeedForward(config)
 
-    def forward(self, queries, keys, values, self_attention_mask, enc_attention_mask, **kwargs):
-        self_att = self.self_attn(queries, queries, queries, attention_mask=self_attention_mask, **kwargs)
-        enc_att = self.enc_attn(self_att, keys, values, attention_mask=enc_attention_mask, **kwargs)
+    def forward(self, queries, keys, values, self_attention_mask, **kwargs):
+        self_att = self.self_attn(queries, keys, values, attention_mask=self_attention_mask, **kwargs)
 
-        ff = self.pwff(enc_att)
+        ff = self.pwff(self_att)
         
         return ff
 
@@ -59,13 +56,20 @@ class Decoder(nn.Module):
         answer_ids = F.pad(answer_ids, (0, self.max_len - answer_ids.size(1)), value=0)                
         #Sử dụng max pooling để giảm kích thước
         encoder_features = F.adaptive_max_pool1d(encoder_features.permute(0, 2, 1), answer_ids.size(1)).permute(0, 2, 1)
+        # Lấy kích thước của encoder_features
+        batch_size, seq_length, feature_dim = encoder_features.size()
+
+        # Chỉ lấy những mask tương ứng với phần tử được max pooling của encoder feature
+        encoder_attention_mask = encoder_attention_mask[:, :seq_length]
+        encoder_attention_mask = encoder_attention_mask[torch.arange(batch_size), encoder_features.argmax(dim=2)]
+        print(encoder_attention_mask.shape)
+
         b_s, seq_len = answer_ids.shape
         answer_padding_masks = generate_padding_mask(answer_ids, padding_idx).to(answer_ids.device)
         answer_self_attention_masks = generate_sequential_mask(seq_len).to(answer_ids.device)
         answer_self_attention_masks = generate_self_attention_masks(answer_padding_masks, answer_self_attention_masks)
         for layer in self.layers:
-            encoder_features = layer(queries=encoder_features, keys=encoder_features, values=encoder_features,
-                                     self_attention_mask=answer_self_attention_masks, enc_attention_mask=encoder_attention_mask)
+            encoder_features = layer(queries=encoder_features, keys=encoder_features, values=encoder_features,self_attention_mask=answer_self_attention_masks)
         encoder_features = self.linear(encoder_features)
         # encoder_attention_mask=self.generate_encoder_mask(encoder_features)
         outputs = self.layer_norm(encoder_features)
